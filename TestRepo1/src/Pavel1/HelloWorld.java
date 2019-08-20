d1d26b9f4c1255a0b8b95631c19e5ae293ed9af9
Pkolarik <pavel.kolarik@t-online.de>
2019-08-20 10:19:30 +0200

    Hello World

---
+++ TestRepo1/src/Pavel1/HelloWorld.java
@@ -0,0 +1,11 @@
+package Pavel1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World...");
+		System.out.print("This is a test java program according to Raghav");
+
+	}
+
+}
